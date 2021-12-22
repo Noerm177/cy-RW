790233f78dd39b52e330716b84d5028215fe3203
Noerm177 <noerm177@gmail.com>
2021-12-22 00:57:30 -0600

    to MAC

--- cypress/integration/Login/Login-As-Admin.spec.js
+++ cypress/integration/Login/Login-As-Admin.spec.js
@@ -1,9 +1,10 @@
 describe('Verify An admin is able to login', () => {
+
     before(() => {
         cy.login('sfasupport@igt.com', 'DullSpongyIde@15')
     });
 
-    after(() => {
+    afterEach(() => {
         cy.get('[ng-if="$ctrl.topmenu.dropdown && $ctrl.session.loggedIn"] > .dropdown-container > .dropdown-toggle > button-label > .app-icon-button > rw-icon > .rw-icon').
         click()
         cy.get('[ng-if="$ctrl.topmenu.logout"] > a').click()
@@ -11,9 +12,13 @@ describe('Verify An admin is able to login', () => {
     
     
     it('Verify Manage User screen was open', () => {
-        cy.wait(2000)
-        cy.get('.page-title.text-truncate.col').invoke('prop','innerText').should('eq', 'Manage Users')
-        
+        cy.wait(3000)
+        cy.get('.page-title.text-truncate.col').then(title => {
+            expect(title).to.equal('Manage User');
+        })
+
+       /* cy.get('.page-title.text-truncate.col')
+            .invoke('prop','innerText').should('eq', 'Manage User')  */
         
     });
 })
