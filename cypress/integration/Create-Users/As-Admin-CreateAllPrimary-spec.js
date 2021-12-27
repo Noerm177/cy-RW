e39a9968b3428a42ebeda099a991b99045754630
Noerm177 <noerm177@gmail.com>
2021-12-27 02:37:21 -0600

    add admin

--- cypress/integration/Create-Users/As-Admin-CreateAllPrimary-spec.js
+++ cypress/integration/Create-Users/As-Admin-CreateAllPrimary-spec.js
@@ -1,4 +1,4 @@
-describe ('Verify that Admin can create all primary users', () => {
+describe('Verify that Admin can create all primary users', () => {
 
     beforeEach(() => {
         cy.login('sfasupport@igt.com', 'DullSpongyIde@15')
@@ -7,42 +7,61 @@ describe ('Verify that Admin can create all primary users', () => {
 
     it('Create Primary Chain Head', () => {
         cy.contains('Create User').click()
-        cy.get(':nth-child(2) > :nth-child(3) > .card > .card-body > .row > .col-2').click()//Click on chain head
-        cy.get('.card-footer > .row > .ml-2 > .rw-button').click({force:true})//next button
+        cy.get(':nth-child(2) > :nth-child(3) > .card > .card-body > .row > .col-2').click() //Click on chain head
+        cy.get('.card-footer > .row > .ml-2 > .rw-button').click({
+            force: true
+        }) //next button
         //Fill out fields for CH
-        cy.CreateUser('automation test', 'LastName', 755000, 'noerm177+chainHeadP@gmail.com', 'noerm177+chainHeadP@gmail.com' )
+        cy.CreateUser('automation test', 'LastName', 755000, 'noerm177+chainHeadP@gmail.com', 'noerm177+chainHeadP@gmail.com')
         cy.wait(1000)
         //Delete retailer
-        cy.DeleteUser(755000)
+        cy.DeleteUser('noerm177+chainHeadP@gmail.com')
         cy.log('Chain Head was create')
         cy.LogOUT()
     });
 
-    it('Create Primary Subordinate', () => { 
+    it('Create Primary Subordinate', () => {
         cy.contains('Create User').click()
-        cy.get(':nth-child(2) > :nth-child(2) > .card > .card-body > .row > .col-2').click()//Click on chain head
-        cy.get('.card-footer > .row > .ml-2 > .rw-button').click({force:true})//next button
+        cy.get(':nth-child(2) > :nth-child(2) > .card > .card-body > .row > .col-2').click() //Click on chain head
+        cy.get('.card-footer > .row > .ml-2 > .rw-button').click({
+            force: true
+        }) //next button
         //Fill out fields for Subordinate 755002
-        cy.CreateUser('automation test', 'LastName', 755002, 'noerm177+subordinateP@gmail.com', 'noerm177+subordinateP@gmail.com' )
+        cy.CreateUser('automation test', 'LastName', 755002, 'noerm177+subordinateP@gmail.com', 'noerm177+subordinateP@gmail.com')
         cy.wait(1000)
         //Delete retailer
-        cy.DeleteUser(755002)
+        cy.DeleteUser('noerm177+subordinateP@gmail.com')
         cy.log('Chain Subordinate was create')
         cy.LogOUT()
     })
 
     it('Create Independent', () => {
         cy.contains('Create User').click()
-        cy.get(':nth-child(2) > :nth-child(1) > .card > .card-body > .row > .col-2').click()//Click on chain head
-        cy.get('.card-footer > .row > .ml-2 > .rw-button').click({force:true})//next button
+        cy.get(':nth-child(2) > :nth-child(1) > .card > .card-body > .row > .col-2').click() //Click on chain head
+        cy.get('.card-footer > .row > .ml-2 > .rw-button').click({
+            force: true
+        }) //next button
         cy.contains('Registration Pin').type(1234)
-        cy.CreateUser('automation test', 'LastName', 752000, 'noerm177+independentP@gmail.com', 'noerm177+independentP@gmail.com' )
+        cy.CreateUser('automation test', 'LastName', 752000, 'noerm177+independentP@gmail.com', 'noerm177+independentP@gmail.com')
         cy.wait(1000)
         //Delete retailer
-        cy.DeleteUser(752000)
+        cy.DeleteUser('noerm177+independentP@gmail.com')
         cy.log('Independent was create')
         cy.LogOUT()
-
     });
-      
+
+    it('Create Adminastrator', () => {
+        cy.contains('Create User').click()
+        cy.get(':nth-child(1) > :nth-child(1) > .card > .card-body > .row > .col-2').click() //Click on chain head
+        cy.get('.card-footer > .row > .ml-2 > .rw-button').click({
+            force: true
+        }) //next button
+        cy.CreateAdmin('automation test', 'LastName', 'noerm177+admin@gmail.com')
+        cy.wait(1000)
+        //Delete retailer
+        cy.DeleteUser('noerm177+admin@gmail.com')
+        cy.log('Admin was create')
+        cy.LogOUT()
+    });
+
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,44 +10,61 @@
 //
 //
 // -- This is a parent command --
-   Cypress.Commands.add('login', (email, password) => {
-       cy.clearCookies()
-       cy.visit('/')
-       cy.wait(1000)
-       cy.get('.login-form > :nth-child(1) > .ng-pristine').type(email)
-       cy.get('.login-form > :nth-child(2) > .ng-pristine').type(password)
-       cy.get('.login-form > :nth-child(3) > .login__button > .rw-button').click()
- })
+Cypress.Commands.add('login', (email, password) => {
+    cy.clearCookies()
+    cy.visit('/')
+    cy.wait(1000)
+    cy.get('.login-form > :nth-child(1) > .ng-pristine').type(email)
+    cy.get('.login-form > :nth-child(2) > .ng-pristine').type(password)
+    cy.get('.login-form > :nth-child(3) > .login__button > .rw-button').click()
+})
 
- Cypress.Commands.add('CreateUser', (FirstN, SecondN, RetailerID, Email, ConfirmE) => {
+Cypress.Commands.add('CreateUser', (FirstN, SecondN, RetailerID, Email, ConfirmE) => {
     cy.contains('First Name').type(FirstN)
     cy.contains('Last Name').type(SecondN)
     cy.get('#create-account-form--retailerId').type(RetailerID)
-    cy.get('#create-account-form--emailAddress').type(Email) 
+    cy.get('#create-account-form--emailAddress').type(Email)
     cy.get('#create-account-form--confirmEmail').type(ConfirmE)
     cy.get('.rw-button.rw-button--contained').first().click()
     //review info
-    cy.get('.modal-footer > [variant="contained"] > .rw-button').should('be.visible').click()  
+    cy.get('.modal-footer > [variant="contained"] > .rw-button').should('be.visible').click()
     cy.get('.modal-content').should('include.text', 'Success!')
+    // cy.get('.rw-button rw-button--contained').click()
+    cy.get('.modal-footer > rw-button > .rw-button').click()
+})
 
-   // cy.get('.rw-button rw-button--contained').click()
-    cy.get('.modal-footer > rw-button > .rw-button').click()    
+Cypress.Commands.add('CreateAdmin', (FirstN, SecondN, Email) => {
+    cy.contains('First Name').type(FirstN)
+    cy.contains('Last Name').type(SecondN)
+    cy.get('#create-account-form--emailAddress').type(Email)
+    cy.get('#create-account-form--confirmEmail').type(Email)
+    cy.get('.rw-button.rw-button--contained').first().click()//submit
+    //review info
+    cy.get('#select_70').click()
+    cy.get('#select_option_74').click() //Roles
+    cy.get('.card-footer > .row > [variant="contained"] > .rw-button').click()
+
+    cy.get('.modal-footer > [variant="contained"] > .rw-button').click() // Review info
+    cy.get('.modal-header').should('include.text', 'Success!')
+    // cy.get('.rw-button rw-button--contained').click()
+    cy.get('.modal-footer > rw-button > .rw-button').click()
 })
 
 //Cypress.Commands.add()
-
-
-
-Cypress.Commands.add('DeleteUser', (RetailerID) => {
-    cy.get('#retailer-number-filter').type(RetailerID)
-    cy.get('.filter-buttons-width > .row > .mr-2 > .rw-button').click()//search button  
+Cypress.Commands.add('DeleteUser', (email) => {
+    cy.get('#email-filter').type(email)
+    cy.get('.filter-buttons-width > .row > .mr-2 > .rw-button').click() //search button  
     cy.wait(1000)
-    cy.get('a.break-word').eq(0).click()// Select user from list
-    cy.get('[ng-if="$ctrl.canUpdateData && $ctrl.canEditAccountDetails"] > a').click({force:true})// click on delete
+    cy.get('a.break-word').eq(0).click() // Select user from list
+    cy.get('[ng-if="$ctrl.canUpdateData && $ctrl.canEditAccountDetails"] > a').click({
+        force: true
+    }) // click on delete
     cy.get('.modal-header').should('include.text', 'Are you sure you want to delete this user?')
     cy.get('.modal-footer > [variant="contained"] > .rw-button').click() // Confirm
     cy.wait(1000)
 })
+//cy.get('#email-filter')
+
 
 Cypress.Commands.add('LogOUT', () => {
     cy.get('[ng-if="$ctrl.topmenu.dropdown && $ctrl.session.loggedIn"] > .dropdown-container > .dropdown-toggle > button-label > .app-icon-button > rw-icon > .rw-icon').
